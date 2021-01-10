690cf3624c1d9a2752fddd1c6b91e53d2300f0fa
Melloware <mellowaredev@gmail.com>
2021-01-10 14:43:40 -0500

    Refactor DatePicker004

--- src/test/java/org/primefaces/extensions/integrationtests/datepicker/DatePicker004Test.java
+++ src/test/java/org/primefaces/extensions/integrationtests/datepicker/DatePicker004Test.java
@@ -21,6 +21,7 @@ import java.time.format.DateTimeFormatter;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -34,6 +35,7 @@ import org.primefaces.extensions.selenium.component.DatePicker;
 public class DatePicker004Test extends AbstractPrimePageTest {
 
     @Test
+    @Order(1)
     @DisplayName("DatePicker: date with time HH:mm:ss. See GitHub #6458 and #6459")
     public void testDateAndTimeWithSeconds(Page page) {
         // Arrange
@@ -58,9 +60,11 @@ public class DatePicker004Test extends AbstractPrimePageTest {
         Assertions.assertEquals("55", timePicker.findElement(By.cssSelector("div.ui-minute-picker > span")).getText());
         // #6458 showSeconds="true" automatically detected because of pattern contains 's'
         Assertions.assertEquals("19", timePicker.findElement(By.cssSelector("div.ui-second-picker > span")).getText());
+        datePicker.hidePanel();
 
         // Assert Submit Value
-        page.button.click();
+        TestUtils.pause(1000);
+        page.submitSeconds.click();
         LocalDateTime newValue = datePicker.getValue();
         Assertions.assertEquals(value, newValue);
         // #6459 showTime="true" automatically detected because of LocalDateTime
@@ -69,6 +73,7 @@ public class DatePicker004Test extends AbstractPrimePageTest {
     }
 
     @Test
+    @Order(2)
     @DisplayName("DatePicker: date with time HH:mm")
     public void testDateAndTimeWithHours(Page page) {
         // Arrange
@@ -91,9 +96,11 @@ public class DatePicker004Test extends AbstractPrimePageTest {
         WebElement timePicker = panel.findElement(By.className("ui-timepicker"));
         Assertions.assertEquals("11", timePicker.findElement(By.cssSelector("div.ui-hour-picker > span")).getText());
         Assertions.assertEquals("55", timePicker.findElement(By.cssSelector("div.ui-minute-picker > span")).getText());
+        datePicker.hidePanel();
 
         // Assert Submit Value
-        page.button.click();
+        TestUtils.pause(1000);
+        page.submitHours.click();
         LocalDateTime newValue = datePicker.getValue();
         Assertions.assertEquals(value, newValue);
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm");
@@ -106,19 +113,23 @@ public class DatePicker004Test extends AbstractPrimePageTest {
         Assertions.assertEquals("mm/dd/yy", cfg.getString("dateFormat"));
         Assertions.assertEquals(defaultDate, cfg.getString("defaultDate"));
         Assertions.assertEquals("single", cfg.getString("selectionMode"));
-        Assertions.assertEquals(true, cfg.getBoolean("showTime"));
+        Assertions.assertTrue(cfg.getBoolean("showTime"));
         Assertions.assertFalse(cfg.getBoolean("inline"));
     }
 
     public static class Page extends AbstractPrimePage {
-        @FindBy(id = "form:dpSeconds")
-        DatePicker datePickerSeconds;
 
-        @FindBy(id = "form:dpHours")
+        @FindBy(id = "form1:dpHours")
         DatePicker datePickerHours;
 
-        @FindBy(id = "form:button")
-        CommandButton button;
+        @FindBy(id = "form1:btnHours")
+        CommandButton submitHours;
+
+        @FindBy(id = "form2:dpSeconds")
+        DatePicker datePickerSeconds;
+
+        @FindBy(id = "form2:btnSeconds")
+        CommandButton submitSeconds;
 
         @Override
         public String getLocation() {
