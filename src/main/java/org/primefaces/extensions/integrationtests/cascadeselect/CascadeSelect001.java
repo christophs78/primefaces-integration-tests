b9eb0215ac966654f9aef2c8dd96e38c233a0cb9
Melloware <mellowaredev@gmail.com>
2021-03-09 11:38:17 -0500

    Remove unused imports

--- src/main/java/org/primefaces/extensions/integrationtests/cascadeselect/CascadeSelect001.java
+++ src/main/java/org/primefaces/extensions/integrationtests/cascadeselect/CascadeSelect001.java
@@ -21,23 +21,20 @@
  */
 package org.primefaces.extensions.integrationtests.cascadeselect;
 
-import lombok.Data;
-import org.primefaces.event.SelectEvent;
-import org.primefaces.extensions.integrationtests.general.model.Driver;
-import org.primefaces.extensions.integrationtests.general.service.RealDriverService;
-import org.primefaces.extensions.integrationtests.general.utilities.TestUtils;
-
-import javax.annotation.PostConstruct;
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
-import javax.faces.model.SelectItem;
-import javax.faces.model.SelectItemGroup;
-import javax.faces.view.ViewScoped;
-import javax.inject.Inject;
-import javax.inject.Named;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.model.SelectItem;
+import javax.faces.model.SelectItemGroup;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+import org.primefaces.event.SelectEvent;
+import org.primefaces.extensions.integrationtests.general.utilities.TestUtils;
+
+import lombok.Data;
 
 @Named
 @ViewScoped
@@ -57,27 +54,27 @@ public class CascadeSelect001 implements Serializable {
         SelectItemGroup nVidia = new SelectItemGroup("nVidia");
         SelectItemGroup nv3000 = new SelectItemGroup("3000-Series");
         SelectItemGroup nv2000 = new SelectItemGroup("2000-Series");
-        nVidia.setSelectItems(new SelectItem[]{nv3000, nv2000});
+        nVidia.setSelectItems(new SelectItem[] {nv3000, nv2000});
 
-        SelectItem nv3090 = new SelectItem( "RTX 3090");
-        SelectItem nv3080 = new SelectItem( "RTX 3080");
+        SelectItem nv3090 = new SelectItem("RTX 3090");
+        SelectItem nv3080 = new SelectItem("RTX 3080");
         SelectItem nv3070 = new SelectItem("RTX 3070");
-        nv3000.setSelectItems(new SelectItem[]{nv3090, nv3080, nv3070});
+        nv3000.setSelectItems(new SelectItem[] {nv3090, nv3080, nv3070});
 
         SelectItem nv2080Ti = new SelectItem("RTX 2080 Ti");
-        SelectItem nv2080 = new SelectItem( "RTX 2080");
+        SelectItem nv2080 = new SelectItem("RTX 2080");
         SelectItem nv2070 = new SelectItem("RTX 3070");
-        nv2000.setSelectItems(new SelectItem[]{nv2080Ti, nv2080, nv2070});
+        nv2000.setSelectItems(new SelectItem[] {nv2080Ti, nv2080, nv2070});
 
         SelectItemGroup amd = new SelectItemGroup("AMD");
         SelectItemGroup amd6000 = new SelectItemGroup("6000-Series");
-        amd.setSelectItems(new SelectItem[]{amd6000});
+        amd.setSelectItems(new SelectItem[] {amd6000});
 
         SelectItem amd6900 = new SelectItem("Radeon 6900");
         SelectItem amd6800xt = new SelectItem("Radeon 6800 XT");
         SelectItem amd6800 = new SelectItem("Radeon 6800");
         SelectItem amd6700 = new SelectItem("Radeon 6700");
-        amd6000.setSelectItems(new SelectItem[]{amd6900, amd6800xt, amd6800, amd6700});
+        amd6000.setSelectItems(new SelectItem[] {amd6900, amd6800xt, amd6800, amd6700});
 
         gpus.add(nVidia);
         gpus.add(amd);
@@ -87,7 +84,7 @@ public class CascadeSelect001 implements Serializable {
         // pause for 1 second to verify AJAX guards are working properly
         TestUtils.wait(1000);
 
-        TestUtils.addMessage("Selected GPU", (String)event.getObject());
+        TestUtils.addMessage("Selected GPU", (String) event.getObject());
     }
 
     public void submit() {
--- src/main/java/org/primefaces/extensions/integrationtests/cascadeselect/CascadeSelect002.java
+++ src/main/java/org/primefaces/extensions/integrationtests/cascadeselect/CascadeSelect002.java
@@ -21,20 +21,20 @@
  */
 package org.primefaces.extensions.integrationtests.cascadeselect;
 
-import lombok.Data;
-import org.primefaces.event.SelectEvent;
-import org.primefaces.extensions.integrationtests.general.utilities.TestUtils;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
 import javax.faces.model.SelectItem;
 import javax.faces.model.SelectItemGroup;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
+
+import org.primefaces.event.SelectEvent;
+import org.primefaces.extensions.integrationtests.general.utilities.TestUtils;
+
+import lombok.Data;
 
 @Named
 @ViewScoped
@@ -54,27 +54,27 @@ public class CascadeSelect002 implements Serializable {
         SelectItemGroup nVidia = new SelectItemGroup("nVidia");
         SelectItemGroup nv3000 = new SelectItemGroup("3000-Series");
         SelectItemGroup nv2000 = new SelectItemGroup("2000-Series");
-        nVidia.setSelectItems(new SelectItem[]{nv3000, nv2000});
+        nVidia.setSelectItems(new SelectItem[] {nv3000, nv2000});
 
         SelectItem nv3090 = new SelectItem(3090, "RTX 3090");
         SelectItem nv3080 = new SelectItem(3080, "RTX 3080");
         SelectItem nv3070 = new SelectItem(3070, "RTX 3070");
-        nv3000.setSelectItems(new SelectItem[]{nv3090, nv3080, nv3070});
+        nv3000.setSelectItems(new SelectItem[] {nv3090, nv3080, nv3070});
 
         SelectItem nv2080Ti = new SelectItem(2081, "RTX 2080 Ti");
         SelectItem nv2080 = new SelectItem(2080, "RTX 2080");
         SelectItem nv2070 = new SelectItem(2070, "RTX 3070");
-        nv2000.setSelectItems(new SelectItem[]{nv2080Ti, nv2080, nv2070});
+        nv2000.setSelectItems(new SelectItem[] {nv2080Ti, nv2080, nv2070});
 
         SelectItemGroup amd = new SelectItemGroup("AMD");
         SelectItemGroup amd6000 = new SelectItemGroup("6000-Series");
-        amd.setSelectItems(new SelectItem[]{amd6000});
+        amd.setSelectItems(new SelectItem[] {amd6000});
 
         SelectItem amd6900 = new SelectItem(6900, "Radeon 6900");
         SelectItem amd6800xt = new SelectItem(6801, "Radeon 6800 XT");
         SelectItem amd6800 = new SelectItem(6800, "Radeon 6800");
         SelectItem amd6700 = new SelectItem(6700, "Radeon 6700");
-        amd6000.setSelectItems(new SelectItem[]{amd6900, amd6800xt, amd6800, amd6700});
+        amd6000.setSelectItems(new SelectItem[] {amd6900, amd6800xt, amd6800, amd6700});
 
         gpus.add(nVidia);
         gpus.add(amd);
@@ -84,7 +84,7 @@ public class CascadeSelect002 implements Serializable {
         // pause for 1 second to verify AJAX guards are working properly
         TestUtils.wait(1000);
 
-        TestUtils.addMessage("Selected GPU", Integer.toString((int)event.getObject()));
+        TestUtils.addMessage("Selected GPU", Integer.toString((int) event.getObject()));
     }
 
     public void submit() {
